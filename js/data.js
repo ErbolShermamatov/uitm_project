67eaf4e7df800da88939156191f0a2b8bd35768c
Erbol <erbol.shermamatov7@gmail.com>
2025-12-11 22:31:10 +0100

    anime icons added

--- js/data.js
+++ js/data.js
@@ -3,19 +3,113 @@ const animeData = [
         title: 'Demon Slayer',
         img: 'img/main/bg/demon_slayer-bg.webp',
         year: 2020,
-        category: 'popular' //sign for filter
+        id: 'demon-slayer',
+        category: 'popular'
     },
     {
         title: 'Chainsaw Man',
         img: 'img/main/bg/chaisawman-bg.webp',
         year: 2022,
-        category: 'popular' //sign for filter
+        id: 'chainsaw-man',
+        category: 'popular'
     },
     {
         title: 'One Piece',
         img: 'img/main/bg/one-piece-bg.webp',
         year: 1999,
-        category: 'ongoing' //sign for filter
+        id: 'one-piece',
+        category: 'ongoing'
+    },
+    {
+        title: 'My Hero Academia',
+        img: 'img/main/bg/my-hero-academia-bg.webp',
+        year: 2016,
+        id: 'my-hero-academia',
+        category: 'ongoing'
+    },
+    {
+        title: 'Gachiakuta',
+        img: 'img/main/bg/gachiakuta-bg.webp',
+        year: 2025,
+        id: 'gachiakuta',
+        category: 'ongoing'
+    },
+    {
+        title: 'To Your Eternity',
+        img: 'img/main/bg/to-your-eternity-bg.webp',
+        year: 2021,
+        id: 'to-your-eternity',
+        category: 'ongoing'
+    },
+    {
+        title: 'Fullmetal Alchemist',
+        img: 'img/main/bg/fullmetal-alchemist-bg.webp',
+        year: 2009,
+        id: 'fullmetal-alchemist',
+        category: 'completed'
+    },
+    {
+        title: 'Stains Gate',
+        img: 'img/main/bg/steins-gate-bg.webp',
+        year: 2011,
+        id: 'stains-gate',
+        category: 'completed'
+    },
+    {
+        title: 'Frieren: Beyond Journey End',
+        img: 'img/main/bg/frieren-bg.webp',
+        year: 2023,
+        id: 'frieren',
+        category: 'ongoing'
+    },
+    {
+        title: 'Hunter X Hunter',
+        img: 'img/main/bg/hunter-x-hunter-bg.webp',
+        year: 2011,
+        id: 'hunter-x-hunter',
+        category: 'completed'
+    },
+    {
+        title: 'Bleach',
+        img: 'img/main/bg/bleach-bg.webp',
+        year: 2004,
+        id: 'bleach',
+        category: 'ongoing'
+    },
+    {
+        title: 'Monster',
+        img: 'img/main/bg/monster-bg.webp',
+        year: 2004,
+        id: 'monster',
+        category: 'completed'
+    },
+    {
+        title: 'The Apothecary Diaries',
+        img: 'img/main/bg/the-apothecary-diaries-bg.webp',
+        year: 2023,
+        id: 'the-apothecary-diaries',
+        category: 'ongoing'
+    },
+    {
+        title: 'Attack on Titan',
+        img: 'img/main/bg/attack-on-titan-bg.webp',
+        year: 2013,
+        id: 'attack-on-titan',
+        category: 'completed'
+    },
+    {
+        title: 'Jujutsu Kaisen',
+        img: 'img/main/bg/jujutsu-kaisen-bg.webp',
+        year: 2020,
+        id: 'jujutsu-kaisen',
+        category: 'ongoing'
+    },
+    {
+        title: 'Bocchi the Rock',
+        img: 'img/main/bg/bochi-the-rock-bg.webp',
+        year: 2022,
+        id: 'bocchi-the-rock',
+        category: 'ongoing'
     },
 ]
 //Принимает массив и отрисовывает его
